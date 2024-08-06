2b746d092fc811e8878872537813979ae68bae0b
abrarshaikkh07 <abrar.devwalla@gmail.com>
2024-08-06 13:43:25 -0400

    list,tuples,dict

--- PythonBasicsw.py
+++ PythonBasicsw.py
@@ -66,16 +66,124 @@
 # print(tu)
 # print(tu.count(1))
 
-print("Enter your favourite movies name: ")
-a=list[input(str("First: "))]
-b=list[input(str("Second: "))]
-c=list[input(str("Third: "))]
-d=list(a+b+c)
-print(a)
-print(b)
-print(c)
-
-print(d)
+# print("Enter your favourite movies name: ")
+# a=[input(str("First: "))]
+# b=[input(str("Second: "))]
+# c=[input(str("Third: "))]
+# print(type(a))
+# d=list(a+b+c)
+# print(d)
+# print(a)
+# print(b)
+# print(c)
+# a.append(b)
+# print(a)
+# print(d)
 # a=[1,2,30]
 # b=[464,5456,5]
 # print(a+b)
+
+# merging list using apend
+# movie=[]
+# mov=input("Enter movie name")
+# movie.append(mov)
+# mov=input("Enter movie name")
+# movie.append(mov)
+# mov=input("Enter movie name")
+# movie.append(mov)
+# print(movie)
+#
+# a=[]
+# a.append(input("Enter movie move name: "))
+# print("\n*******************************")
+# a.append(input("Enter movie move name: "))
+# print("\n*******************************")
+# a.append(input("Enter movie move name: "))
+# print(a)
+#
+# b=list[a.reverse()]
+# print(b)
+# print(type(b))
+
+# a=len(input("enter "))
+# print(a)
+#
+
+
+# a.rever
+# print(a)
+#
+# a=[1,2,1]
+# # a=[input("Enter a list")]
+# b=a.reverse()
+# print(b)
+#
+# if a==b:
+#     print("this is palindrome list")
+
+# # list copy
+# a=[]
+# a.append(input("Enter movie move name: "))
+# print("\n*******************************")
+# a.append(input("Enter movie move name: "))
+# print("\n*******************************")
+# a.append(input("Enter movie move name: "))
+# print(a)
+# copy_a=a.copy()
+# copy_a.reverse()
+# if copy_a == a:
+#     print("This is palindrome list"
+#           "")
+
+# a=[1,2]
+# a.reverse()
+# b=a
+#
+# print(b)
+
+#dict program
+
+# dic={}
+# a=input("enter subject name :")
+# b=input("Enter Marks ")
+# dic[a]=b
+#
+# a=input("enter subject name :")
+# b=input("Enter Marks ")
+# dic[a]=b
+# a=input("enter subject name :")
+# b=input("Enter Marks ")
+# dic[a]=b
+#
+# print(dic)
+
+# set program
+#
+# s=set()
+# print(type(s))
+# a=input("Enter")
+# s.add(a)
+# a=input("Enter")
+# s.add(a)
+# a=input("Enter")
+# s.add(a)
+# print(s)
+
+#tupel program
+# s=()
+# print(type(s))
+# a=input("Enter")
+# b=input("Enter")
+#
+# c=input("Enter")
+# s=(a,b,c)
+# print(s)
+
+#Dict
+a={
+    'subject': 'ma',
+     'fdfs': ' fdsf'
+
+
+}
+print(a)
