7d63699ced3d07bd0fa26db39ef86e54278d8d96
abrarshaikkh07 <abrar.devwalla@gmail.com>
2024-08-06 01:36:21 -0400

    list methods

--- PythonBasicsw.py
+++ PythonBasicsw.py
@@ -1,4 +1,63 @@
-l=[1,2,3]
-print(l)
-print(l.append(4))
-print(l)
+    # # # ## l=[1,2,3]
+    # # # # # print(l)
+    # # # # # print(l.append(4))
+    # # # # # print(l)
+    # # # # # print(l[0])
+    # # # # # m=[8,5,9,4,6]
+    # # # # # print(m)
+    # # # # # (m.sort())
+    # # # # # print(m)
+    # # # # # (m.sort(reverse=True))
+    # # # # # print(m)
+    # # # # # oreder for character list
+    # # # # oc=['apple','orange','banana']
+    # # # # print(oc)
+    # # # # oc.sort()
+    # # # # print(oc)
+    # # # # oc.sort(reverse=True)
+    # # # # print(oc)
+    # # # #
+    # # # #reverse list
+    # # # # rl=[1,2,3,4,5]
+    # # # # print(rl)
+    # # # # rl.reverse()
+    # # # # print(rl)
+    # # # # # insert in list'
+    # # # # il=['a','b','c','d']
+    # # # # print(il)
+    # # # # il.insert(0,1)
+    # # # # print(il)
+    # # #
+    # # # #index method
+    # # # im = ['a', 'b', 'c', 'd']
+    # # # print(im)
+    # # # p=im.index('b')
+    # # # print([p])
+    # # #
+    # # # #remove method //remove target attribute when it comes first only l
+    # # # rm=[1,2,3,5]
+    # # # print(rm)
+    # # # rm.remove(2)
+    # # # print(rm)
+    # #
+    # # # pop method ; to delete attribute according to index
+    # # pm=[2,59,7,9]
+    # # print(pm)
+    # # pm.pop(0)
+    # # print(pm)
+    # # # copy method
+    # # x=pm.copy()
+    # # print(x)
+    # #
+    # #
+    # # #reverse method
+    # # y=x.reverse()
+    # # print(x)
+    # #
+    # # extend method
+    # x=[1,2,3,4]
+    # y=[5,6,7,8,9]
+    # x.extend(y)
+    # print(x)
+    #
+    # add method
